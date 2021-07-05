ff071fd82333bbafb3a1750568a2cbe11317372a
zhenghui <zhenghui@zinete.com>
2021-07-05 17:13:54 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by zhenghui on 2021/7/5.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
